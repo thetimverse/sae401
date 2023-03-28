ebe2c16e5a880a47011d3486e54a1a1e196337f8
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-03-28 12:07:33 +0200

    decoupage

---
+++ site_jeu/src/components/Score.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <section class="score">
+    <div>
+      <h1>15</h1>
+      <img src="src/assets/img/shield_card_me.svg" alt="">
+    </div>
+    <div>
+      <h1>9</h1>
+      <img src="src/assets/img/marvel_me.svg" alt="">
+    </div>
+  </section>
+</template>
